ee8e5cd245655d11149b5b3c61257d9fd37a48b2
Thomson <779387850@qq.com>
2017-05-04 16:52:09 +0800

    在线编辑记事本

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+
+<template>
+  <div class="page" id="app">
+    <header>
+      <Topbar/>
+    </header>
+    <main>
+      <ResumeEditor/>
+      <ResumePreview/>
+    </main>
+  </div>
+</template>
+
+<script>
+import './assets/reset.css'
+
+import Topbar from './components/Topbar'
+import ResumeEditor from './components/ResumeEditor'
+import ResumePreview from './components/ResumePreview'
+
+export default {
+  name: 'app',
+  data:function(){
+    return{
+      text:'你好'
+    }
+
+  },
+  components: {Topbar, ResumeEditor, ResumePreview}
+
+}
+</script>
+
+<style>
+  .page{
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    background: #EAEBEC;
+  }
+
+  .page>main{
+    flex-grow: 1;  
+  }
+
+  .page>main{
+    min-width: 1024px;
+    max-width: 1440px;
+    margin-top: 16px;
+    margin-bottom: 16px;
+    padding: 0 16px;
+    display: flex;
+    justify-content: space-between;
+    align-self: center;
+    width: 100%;
+  }
+  
+  #resumeEditor{
+    width: 35%;
+    background:#ffffff;
+    box-shadow:0 1px 3px 0 rgba(0,0,0,0.25);
+  }
+  #resumePreview{
+    flex-grow: 1;
+    background:#ffffff;
+    margin-left: 16px;
+    box-shadow:0 1px 3px 0 rgba(0,0,0,0.25);
+  }
+  
+</style>
