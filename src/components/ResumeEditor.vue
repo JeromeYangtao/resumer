301c3dd4818ee29f033e2b5a0bfe9e5f2392f83c
Thomson <779387850@qq.com>
2017-05-07 22:19:17 +0800

    添加pannel

--- src/components/ResumeEditor.vue
+++ src/components/ResumeEditor.vue
@@ -13,7 +13,19 @@
     </nav>
     <ol class="panels">
      <li v-for="item in resume.config" v-show="item.field === selected">
-        {{resume[item.field]}}
+       <div v-if="resume[item.field] instanceof Array">
+          <div class="subitem" v-for="subitem in resume[item.field]">
+            <div class="resumeField" v-for="(value,key) in subitem">
+              <label> {{key}} </label>
+              <input type="text" :value="value">
+            </div>
+            <hr>
+          </div>
+        </div>
+       <div v-else class="resumeField" v-for="(value,key) in resume[item.field]">
+         <label> {{key}} </label>
+         <input type="text" v-model="resume[item.field][key]">
+       </div>
       </li>
     </ol>   
    </div>
@@ -39,11 +51,26 @@
             city: '',
             title: ''
           },
-          'work history': [],
-          education: [],
-          projects: [],
-          awards: [],
-          contacts: []
+          'work history': [
+            {company: 'AL', content: '我的第二份工作是'},
+            {company: 'TX', content: '我的第一份工作是'},
+          ],
+          education: [
+            { school: 'AL', content: '文字' },
+            { school: 'TX', content: '文字' },
+          ],
+          projects: [
+            { name: 'project A', content: '文字' },
+            { name: 'project B', content: '文字' },
+          ],
+          awards: [
+            { name: 'awards A', content: '文字' },
+            { name: 'awards B', content: '文字' },
+          ],
+          contacts: [
+            { contact: 'phone', content: '13812345678' },
+            { contact: 'qq', content: '12345678' },
+          ]
         }
       }
   }
@@ -77,6 +104,12 @@
         }
       }
     }
+    >.panels{
+      flex-grow: 1;
+      > li {
+        padding: 24px;
+      }
+    }
   }
   svg.icon{
       width: 24px; 
@@ -84,5 +117,23 @@
   }
   ol{
     list-style: none;
+  }
+  .resumeField{
+    > label{
+      display: block;
+    }
+    input[type=text]{
+      margin: 16px 0;
+      border: 1px solid #ddd;
+      box-shadow:inset 0 1px 3px 0 rgba(0,0,0,0.25);
+      width: 100%;
+      height: 40px;
+      padding: 0 8px;
+    }
+  }
+  hr{
+    border: none;
+    border-top: 1px solid #ddd;
+    margin: 24px 0;
   }
 </style>
