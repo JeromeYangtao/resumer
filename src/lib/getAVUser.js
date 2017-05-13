ec2a147c30bf2d25ee7a5e3f487956d55735f137
Thomson <779387850@qq.com>
2017-05-13 11:22:17 +0800

    data中属性值不为null和undefined

--- src/lib/getAVUser.js
+++ src/lib/getAVUser.js
@@ -1,5 +1,9 @@
 import AV from '../lib/leancloud'
 export default function(user) {
     var { id, attributes: { username } } = user || AV.User.current() || { attributes: {} }
-    return { id, username }
+    return {
+        // 不能让data中的属性值为undefined或null，否则报错。原因似乎与Object.defineProperty有关
+        id: id || '',
+        username: username || ''
+    }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -79,7 +79,7 @@ export default new Vuex.Store({
                 // console.log(state.user)
         },
         removeUser(state) {
-            state.user.id = null
+            state.user.id = ''
         }
     }
 })
