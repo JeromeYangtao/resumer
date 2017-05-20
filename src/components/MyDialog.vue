b4313a648567417da33ad13d3a0fe1491003df4b
Thomson <779387850@qq.com>
2017-05-20 11:51:15 +0800

    预览

--- src/components/MyDialog.vue
+++ src/components/MyDialog.vue
@@ -44,7 +44,67 @@
     justify-content: center;
     align-items: center;
   }
-  .dialog{
+  @media (min-width:200px){
+    .dialog{
+    background: linear-gradient(to bottom, rgba(146, 135, 187, 0.8) 0%, rgba(0, 0, 0, 0.6) 100%);
+    min-height: 4em;
+    height: 100%;
+    min-width: 100vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    > header{
+      padding-right: 16px;
+      display: flex;
+      justify-content: flex-end;
+      position: relative;
+      >.form-logo {
+          position: absolute;
+          top: 13rem;
+          left: 10rem;
+          min-width: 12rem;
+          height: 2rem;
+          background: #fff;
+          transform-origin: 0 100%;
+          transform: rotate(-45deg);
+         }
+      >.form-logo:before {
+          content: "";
+          position: absolute;
+          left: 0;
+          bottom: 100%;
+          width: 2rem;
+          height: 5.2rem;
+          background: #fff;
+          box-shadow: inset -0.2rem -2rem 2rem rgba(0, 0, 0, 0.2);
+         }
+      >.close{
+          cursor: pointer;
+          border: 2px solid white;
+          min-width: 30px;
+          min-height: 26px;
+          text-align: center;
+          vertical-align: middle;
+          line-height: 26px;
+          border-radius: 2px;
+          transition: 0.6s;
+        }
+      > .close:hover{
+          background: white;
+          color: #0D90F0;
+      }
+    }
+    > main{
+      padding: 16px;
+      >div{
+        margin-top: .5em;
+        text-align: center;
+      }
+    }
+  }
+  }
+  @media (min-width: 500px){
+    .dialog{
     background: linear-gradient(to bottom, rgba(146, 135, 187, 0.8) 0%, rgba(0, 0, 0, 0.6) 100%);
     min-height: 4em;
     height: 100%;
@@ -101,11 +161,5 @@
       }
     }
   }
-  @media (max-width: 500px){
-    .dialog{
-      width: 1000px;
-      height: 100vh;
-      background: red;
-    }
   }
 </style>
